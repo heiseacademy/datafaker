ea4d8be8f650d0afc3aebe263b3d48f508395e76
Sandra Parsick <mail@sandra-parsick.de>
2023-08-11 08:37:50 +0200

    sample datafaker sequence

--- 05-datafaker-fake-sequence/src/test/java/com/github/sparsick/heise/datafaker/FakeSequenceTest.java
+++ 05-datafaker-fake-sequence/src/test/java/com/github/sparsick/heise/datafaker/FakeSequenceTest.java
@@ -1,11 +1,78 @@
 package com.github.sparsick.heise.datafaker;
 
+import net.datafaker.Faker;
+import net.datafaker.sequence.FakeSequence;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 public class FakeSequenceTest {
 
     @Test
     void sequenceTest() {
+        Faker faker = new Faker();
+
+        List<String> names = faker.collection(
+                () -> faker.dcComics().hero(),
+                () -> faker.funnyName().name()
+        ).len(5).generate();
     }
+
+    @Test
+    void randomTypSequence() {
+        Faker faker = new Faker();
+
+        List<Object> randomList = faker.<Object>collection(
+                () -> faker.dcComics().hero(),
+                () -> faker.random().nextInt(100)
+        ).len(5).generate();
+    }
+
+    @Test
+    void sequenceWithNull() {
+        Faker faker = new Faker();
+
+        List<Object> randomList = faker.<Object>collection(
+                () -> faker.dcComics().hero(),
+                () -> faker.random().nextInt(100)
+        ).nullRate(0.3).len(5).generate();
+    }
+
+    @Test
+    void sequenceAsStreamTest() {
+        Faker faker = new Faker();
+
+        Stream<String> names = faker.stream(
+                () -> faker.dcComics().hero(),
+                () -> faker.funnyName().name()
+        ).len(5).generate();
+    }
+
+    @Test
+    void infinitySequence() {
+        Faker faker = new Faker();
+
+        FakeSequence<String> infiniteNameStream = faker.stream(
+                () -> faker.dcComics().hero(),
+                () -> faker.funnyName().name()
+        ).build();
+
+        infiniteNameStream.isInfinite(); // true
+    }
+
+    @Test
+    void finitySequence() {
+        Faker faker = new Faker();
+
+        FakeSequence<String> infiniteNameStream = faker.collection(
+                () -> faker.dcComics().hero(),
+                () -> faker.funnyName().name()
+        ).build();
+
+        infiniteNameStream.isInfinite(); // false
+    }
+
+
 
 }
